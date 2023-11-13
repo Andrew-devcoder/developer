cead8de1b5f1c65b4931800d5a3854d6ffdcd479
Andrew-devcoder <andrew.kovpak.01@gmail.com>
2023-11-13 21:58:08 +0200

    draft

---
+++ src/components/code/Code.jsx
@@ -0,0 +1,51 @@
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+
+import style from './Code.module.scss'
+
+const Code = () => {
+
+	const content = `
+	
+	.header {
+		display: flex;
+
+	}
+	
+	`
+
+
+
+	const [text] = useTypewriter({
+		words: [content],
+		loop: {},
+		deleteSpeed: 2
+	})
+
+	return (
+		<>
+			<div className={style.Layout}>
+				<div className={style.container}>
+					<div className={style.header}>
+						<div className={style.circul}></div>
+						<div className={style.circul}></div>
+						<div className={style.circul}></div>
+					</div>
+					<div className={style.main}>
+						<p>
+							<span>
+								{text}
+							</span>
+
+
+							{/*  */}
+
+							<Cursor />
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Code
