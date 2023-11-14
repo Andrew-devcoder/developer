7c045607b8917d48c7a9b4359738d42f5565d511
Andrew-devcoder <andrew.kovpak.01@gmail.com>
2023-11-14 16:48:25 +0200

    little change style

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,21 +1,15 @@
-import Button from '../button/Button'
 import Card from './card/Card'
 import style from './Cards.module.scss'
 
 
 const Cards = () => {
 
-	const styleItem1 = style.item1
-	const styleItem2 = style.item2
-	const styleItem3 = style.item3
-	const styleItem4 = style.item4
-
 	return (
 		<>
 			<div className={style.layout}>
 
 				<div className={style.container}>
-					<div className={style.item1}>
+					<div className={style.item}>
 						<h2>Low-Code- und No-Code-Optionen, damit Sie gleich loslegen können</h2>
 						<p>
 							Millionen von Unternehmen aller Größenordnungen nutzen Stripe, um Zahlungen online und persönlich zu akzeptieren, Auszahlungen vorzunehmen, Finanzprozesse zu automatisieren und letztendlich ihren Umsatz zu steigern.
