c824b6b71a9317f5291ea5801ca58174085da866
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-11-23 18:50:32 +0200

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import removeAttr from "remove-attr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
