ff941743a267dcd22f9fed78b0f94acab2f4e6b6
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-11-24 15:03:32 +0200

    fix and remove

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import removeAttr from "remove-attr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // plugins: [react()],
-    plugins: [
-        react(),
-        // removeAttr({
-        //     extensions: ["jsx"],
-        //     attributes: [
-        //         "playsinline",
-        //         "muted",
-        //         "webkit-playsinline",
-        //         "preload",
-        //     ],
-        //     ignoreFolders: ["src/pages", "src/components"],
-        //     ignoreFiles: ["src/pages/screen/about/About.jsx"],
-        // }),
-    ],
+    plugins: [react()],
 });
